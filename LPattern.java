b51fd5e1001310f5e934f20a02dc105421fa0396
YashPandey99 <yash.pandey_cs18@gla.ac.in>
2021-02-04 00:27:06 +0530

    LPattern

---
+++ LPattern.java
@@ -0,0 +1,19 @@
+import java.util.*
+;public class LPattern {
+
+    public static void main(String[] args) {
+        for(int j=0;j<10;j++){
+            for(int i=0;i<3;i++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+    }
+        for(int x=7;x<10;x++){
+            for(int y=0;y<10;y++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+    }
+
+}
+}
