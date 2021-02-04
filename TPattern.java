8fefc02e698e80a0f703ca76547777cbd869a4fd
YashPandey99 <yash.pandey_cs18@gla.ac.in>
2021-02-05 00:30:50 +0530

    T Pattern

---
+++ TPattern.java
@@ -0,0 +1,20 @@
+import java.util.*
+;public class TPattern {
+
+    public static void main(String[] args) {
+        for(int j=0;j<3;j++){
+            for(int i=0;i<21;i++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+    }
+        for(int b=3;b<15;b++){
+            System.out.print("               ");
+            for(int c=11;c<14;c++){
+                System.out.print(" *");
+                }
+
+            System.out.println("");
+        }
+}
+}
